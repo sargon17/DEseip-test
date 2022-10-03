4f162ac8b398201dba826bc5d3247e15fec08a77
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-10-03 19:31:53 +0200

    setting up the project

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import "./scss/app.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <h2>Hello World</h2>
+      <h3>Hello World</h3>
+      <h4>Hello World</h4>
+      <h5>Hello World</h5>
+      <h6>Hello World</h6>
+      <p className="subtitle1">Hello World</p>
+      <p className="subtitle2">Hello World</p>
+      <p className="body1">Hello World</p>
+      <p className="body2">Hello World</p>
+      <p className="body3">Hello World</p>
+      <p className="body4">Hello World</p>
+      <p className="accent">Hello World</p>
+    </div>
+  );
+}
+
+export default App;
