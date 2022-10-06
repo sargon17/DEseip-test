1684cb665a6c57187316835a6862a51904f44259
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-10-06 16:27:35 +0200

    Siper implemented

--- src/pages/sections/Services.jsx
+++ src/pages/sections/Services.jsx
@@ -89,31 +89,6 @@ export default function Services() {
                 </div>
               </SwiperSlide>
             </Swiper>
-
-            {/* <ul className="flex flex-row flex-wrap items-baseline justify-center">
-              <li className="w-4/12 text-center  gap-4">
-                <img src={esecutore} alt="messages" className="mx-auto mb-8" />
-                <p className="body1">Nomina di un esecutore</p>
-              </li>
-              <li className="w-4/12 text-center  gap-4">
-                <img src={buildings} alt="messages" className="mx-auto mb-8" />
-                <p className="body1">
-                  Apertura di rapporti intestati a società e fiduciarie
-                </p>
-              </li>
-              <li className="w-4/12 text-center  gap-4">
-                <img src={hands} alt="messages" className="mx-auto mb-8" />
-                <p className="body1">Apertura di rapporti cointestati</p>
-              </li>
-              <li className="w-4/12 text-center  gap-4">
-                <img
-                  src={generations}
-                  alt="messages"
-                  className="mx-auto mb-8"
-                />
-                <p className="body1">Passaggio generazionale</p>
-              </li>
-            </ul> */}
           </div>
         </div>
       </div>
